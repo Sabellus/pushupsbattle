05d5f81f3aa1120018cd623b82fb163586d42b8e
Sabellus <nakazan.ru@gmail.com>
2018-06-07 03:06:16 +0500

    add regexp validation js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,22 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+document.addEventListener('DOMContentLoaded', () => {
+  var el = document.getElementById('email');
+  el.addEventListener('change', (event) => {
+    var val = event.target.value;
+    var reg =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    var mess=reg.test(val);
+    if (mess)
+    {
+      console.log("кок")
+    }
+    else
+    {
+      alert("Введите корректные данные")
+    }
+  })
+})
